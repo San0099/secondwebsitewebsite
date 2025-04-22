289f2c6e23b616ec079a875cc2c6ca12919e7184
San0099 <sanamulla911@gmail.com>
2025-04-22 17:46:32 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  images: {
+    domains: ['images.unsplash.com', 'source.unsplash.com', 'plus.unsplash.com', 'pixabay.com', 'cdn.pixabay.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
